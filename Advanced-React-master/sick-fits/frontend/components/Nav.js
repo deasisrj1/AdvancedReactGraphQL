2c7777966321ccc28afbddd2645dd2c2ae33f10a
deasisrj1 <rdeasis@ualberta.ca>
2019-01-18 07:11:26 -0700

    46/68

--- Advanced-React-master/sick-fits/frontend/components/Nav.js
+++ Advanced-React-master/sick-fits/frontend/components/Nav.js
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import {Mutation } from 'react-apollo';
+import { Mutation } from 'react-apollo';
 import { TOGGLE_CART_MUTATION } from '../components/Cart';
 import NavStyles from './styles/NavStyles';
 import User from './User';
@@ -8,45 +8,43 @@ import CartCount from './CartCount';
 
 const Nav = () => (
     <User>
-            {({data: { me }}) => (
-        <NavStyles>
+        {({ data: { me } }) => (
+            <NavStyles>
 
-            <Link href="/items ">
-                <a>Shop</a>
-            </Link>
+                <Link href="/items ">
+                    <a>Shop</a>
+                </Link>
 
-            {me && (
-                <>
-            <Link href="/sell">
-                <a>Sell</a>
-            </Link>
-            <Link href="/orders">
-                <a>Orders</a>
-            </Link>
-            <Link href="/me">
-                <a>Account</a>
-            </Link>
-            <Mutation mutation={TOGGLE_CART_MUTATION}>
-            {(toggleCart) => (
+                {me && (
+                    <>
+                        <Link href="/sell">
+                            <a>Sell</a>
+                        </Link>
+                        <Link href="/orders">
+                            <a>Orders</a>
+                        </Link>
+                        <Link href="/me">
+                            <a>Account</a>
+                        </Link>
+                        <Mutation mutation={TOGGLE_CART_MUTATION}>
+                            {(toggleCart) => (
+                                <button onClick={toggleCart}>My Cart <CartCount count={me.cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0)}></CartCount></button>
+                            )}
+                        </Mutation>
+                        <Signout />
+                    </>
+                )}
 
-            <button onClick={toggleCart}>My Cart <CartCount count={me.cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0)}></CartCount></button>
-            )}
-            </Mutation>
-            <Signout/>
+                {!me && (
 
-            </>
-            )}
+                    <Link href="/signup">
+                        <a>Sign In</a>
+                    </Link>
+                )}
 
-            {!me && (
-
-            <Link href="/signup">
-                <a>Sign In</a>
-            </Link>
-            )}
-    
-        </NavStyles>
-            )}
-        </User>
+            </NavStyles>
+        )}
+    </User>
 )
 
 export default Nav;
